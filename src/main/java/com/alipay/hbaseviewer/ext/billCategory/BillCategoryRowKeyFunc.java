bbefc43fd0c02f579bad855374a0c93ac1d0c331
xinzhi.zhang <xinzhi.zhang@alipay.com>
2014-03-11 20:28:07 +0800

    ebill

---
+++ src/main/java/com/alipay/hbaseviewer/ext/billCategory/BillCategoryRowKeyFunc.java
@@ -0,0 +1,53 @@
+package com.alipay.hbaseviewer.ext.billCategory;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import com.alipay.simplehbase.client.RowKey;
+import com.alipay.simplehbase.client.rowkey.BytesRowKey;
+import com.alipay.simplehbase.client.rowkeytextfun.RowKeyTextFunc;
+import com.alipay.simplehbase.exception.SimpleHBaseException;
+import com.alipay.simplehbase.util.BytesUtil;
+import com.alipay.simplehbase.util.StringUtil;
+
+public class BillCategoryRowKeyFunc implements RowKeyTextFunc {
+
+    @Override
+    public RowKey func(String text) {
+        try {
+            String[] parts = text.split("_");
+            String userId = parts[0];
+            String billCycle = parts[1];
+
+            // userId进行MD5加密
+            String userIdMD5 = DigestUtils.md5Hex(userId.getBytes("utf-8"));
+
+            // md5前4位
+            byte[] userIdMD5Byte = Bytes.toBytes(userIdMD5.substring(0, 4));
+
+            // userId
+            byte[] userIdByte = Bytes.toBytes(StringUtil.reverse(userId));
+
+            // 账期
+            int reverseCycle = 100000000 - Integer.parseInt(billCycle);
+            byte[] cycleByte = Bytes.toBytes(reverseCycle + "");
+
+            return new BytesRowKey(BytesUtil.merge(userIdMD5Byte,
+                    Bytes.toBytes("_"), userIdByte, Bytes.toBytes("_"),
+                    cycleByte));
+        } catch (Exception e) {
+            throw new SimpleHBaseException(e);
+        }
+    }
+
+    @Override
+    public String funcName() {
+        return "billCategoryRowKey";
+    }
+
+    @Override
+    public String desc() {
+        return "billCategoryRowKey()";
+    }
+
+}
---
+++ src/main/java/com/alipay/hbaseviewer/ext/billCategory/BillCategoryRowKeyHandler.java
@@ -0,0 +1,21 @@
+package com.alipay.hbaseviewer.ext.billCategory;
+
+import org.apache.hadoop.hbase.util.Bytes;
+
+import com.alipay.simplehbase.client.rowkey.handler.RowKeyHandler;
+import com.alipay.simplehbase.util.BytesUtil;
+import com.alipay.simplehbase.util.StringUtil;
+
+public class BillCategoryRowKeyHandler implements RowKeyHandler {
+    public Object convert(byte[] key) {
+        StringBuilder sb = new StringBuilder();
+        String reverseUserId = Bytes.toString(BytesUtil.subBytes(key, 5, 16));
+        String userId = StringUtil.reverse(reverseUserId);
+        sb.append(userId);
+        sb.append("_");
+        String reverseCycle = Bytes.toString(BytesUtil.subBytes(key, 22, 8));
+        int cycle = 100000000 - Integer.parseInt(reverseCycle);
+        sb.append(cycle);
+        return sb.toString();
+    }
+}
